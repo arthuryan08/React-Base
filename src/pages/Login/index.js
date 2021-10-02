8de0f346e7246e38b91031344ded1df74c96d365
Arthur Yan <arthuryan08@gmail.com>
2021-10-02 12:01:31 -0300

    Redux config
    Create the store folder and initial config the provider, reducer, actions, initial state.
    learned how to use useSelector and useDispatch

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,18 +1,26 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles'
 import { Title, Paragrafo } from './styled'
 
-import axios from '../../services/axios'
-
 export default function Login() {
+  const dispatch = useDispatch()
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO'
+    })
+  }
 
   return (
     <>
     <Container>
       <Title>Login</Title>
       <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
-      <button>Enviar</button>
+      <button onClick={handleClick}>Enviar</button>
     </Container>
     </>
   )
