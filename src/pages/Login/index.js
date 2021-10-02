f22d5781381d19bf823c57942afec7f96f90b703
Arthur Yan <arthuryan08@gmail.com>
2021-10-02 05:08:03 -0300

    add and configure axios

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import { Container } from '../../styles/GlobalStyles'
 import { Title, Paragrafo } from './styled'
 
+import axios from '../../services/axios'
+
 export default function Login() {
+
+  useEffect(()=> {
+    async function getData(){
+      const response = await axios.get('/alunos')
+      const { data } = response;
+        console.log(data)
+    }
+
+    getData()
+  }, [])
   return (
     <>
     <Container>
