6646f1f1f6dc7e8a215640c12f16fc69b1af72e9
Arthur Yan <arthuryan08@gmail.com>
2021-10-01 21:45:00 -0300

    Styled Components and GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import Login from './pages/Login'
+import React from "react";
+
+import Login from "./pages/Login";
+import GlobalStyle from "./styles/GlobalStyles";
 
 function App() {
   return (
-    <Login/>
+    <>
+      <Login />
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Container } from '../../styles/GlobalStyles'
+import { Title, Paragrafo } from './styled'
 
 export default function Login() {
-  return <h1>Login</h1>
+  return (
+    <>
+    <Container>
+      <Title>Login</Title>
+      <Paragrafo>Lorem ipsum dolor sit amet.</Paragrafo>
+    </Container>
+    </>
+  )
 }
